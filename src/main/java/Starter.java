b2cf5d2e706fdece17cb6211861fcc6606cdf009
radoslavvlaskovski <radoslav.vlaskovsky@gmail.com>
2018-01-10 10:02:29 +0100

    added starter

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,9 +1,30 @@
+import org.openbaton.plugin.PluginStarter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * Created by rados on 12/17/2017.
  */
 public class Starter {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws NoSuchMethodException, InterruptedException, IOException,
+            InstantiationException, TimeoutException, IllegalAccessException, InvocationTargetException {
+        Logger log = LoggerFactory.getLogger(Starter.class);
+        if (args.length == 4) {
+            PluginStarter.registerPlugin(
+                    PrometheusMonitoringAgent.class,
+                    args[0],
+                    args[1],
+                    Integer.parseInt(args[2]),
+                    //          Integer.parseInt(args[3]));
+                    1);
+        } else {
+            PluginStarter.registerPlugin(PrometheusMonitoringAgent.class, "prometheus", "localhost",
+                    5672, 1);
+        }
     }
 }
